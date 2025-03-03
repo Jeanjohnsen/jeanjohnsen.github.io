39701962d9aae1072c79d243139328c59815f864
Jean Johnsen <jeanjohnsen@Jeans-MacBook-Pro.local>
2025-03-03 13:09:08 +0100

    added 3d designs section

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     const timelineItems = document.querySelectorAll(".timeline-item");
+    const rootStyles = getComputedStyle(document.documentElement);
+    const primaryColor = "#00DAC6"; // Highlight color
+    const backgroundColor = rootStyles.getPropertyValue('--body-bg').trim();
+    const textColor = rootStyles.getPropertyValue('--body-color').trim();
+    const dateColor = "#c2c1c1"; // Lighter color for the years
+    
     timelineItems.forEach((item, index) => {
         setTimeout(() => {
             item.classList.add("visible");
+            item.style.borderLeft = `3px solid ${primaryColor}`;
+            item.style.backgroundColor = backgroundColor;
+            item.style.color = textColor;
+            item.querySelector(".timeline-date").style.color = dateColor; // Set lighter color for dates
         }, index * 300);
     });
+    
 });
