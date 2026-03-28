65434e0579246b6221fbf4631a0e6dfa8ea9d068
Jeanjohnsen <jeanjohnsen@pm.me>
2026-03-28 14:04:42 +0100

    d

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const timelineItems = document.querySelectorAll(".timeline-item");
     const rootStyles = getComputedStyle(document.documentElement);
-    const primaryColor = "#00DAC6"; // Highlight color
-    const backgroundColor = rootStyles.getPropertyValue('--body-bg').trim();
-    const textColor = rootStyles.getPropertyValue('--body-color').trim();
-    const dateColor = "#c2c1c1"; // Lighter color for the years
-    
-    timelineItems.forEach(item => {
-        item.style.borderLeft = `3px solid ${primaryColor}`;
-        item.style.backgroundColor = backgroundColor;
-        item.style.color = textColor;
-        if (item.querySelector(".timeline-date")) {
-            item.querySelector(".timeline-date").style.color = dateColor;
-        }
+    const bg   = rootStyles.getPropertyValue('--body-bg').trim();
+    const text = rootStyles.getPropertyValue('--body-color').trim();
+
+    document.querySelectorAll(".timeline-item, .project-card").forEach(el => {
+        el.style.backgroundColor = bg;
+        el.style.color = text;
     });
-    
 });
